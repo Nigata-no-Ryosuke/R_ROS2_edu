91e9799ae5103f7418b81f8a59ebbc419d7631c2
Nigata-no-Ryosuke <iryopan1182@gmail.com>
2024-02-16 07:59:30 +0000

    ver1

---
+++ p_eva_15_ir/src/random_test.py
@@ -0,0 +1,42 @@
+import random
+
+class Eva15():
+    # 左打ち(1/319で当たる）
+    #
+    current_credit = 20000
+
+    def __init__(self, init_balance, ball_price=4):
+        self.credit = 20000 #First total credit
+        self.ball_price =ball price
+        self.balls = init_balance/ball_price
+
+
+    #(1/319)
+    def hidariuti():
+        print(逃げちゃだめだ)
+        if random.randrange(319)==1:
+            return ture
+        else: return false
+    　　　　
+    #(1/319) 100回転
+    def chanceTime():
+        if self.hidariuti():#rush直行
+            pass
+
+        else:#時短100回
+            return false
+        self.add_balls ==450
+        if rand(random.ramdom(),2) ==
+
+
+
+    # ST(1/99) 164回転
+    def rush():
+    
+
+if __name__=="__main__":
+    print(投資は？)
+    input_balnce = input("Indical Balance Amount")
+    print(f'your balance:{input_balance}')
+
+
